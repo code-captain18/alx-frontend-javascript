ea0bfbc48a350ffacc064510aa8c773c834f262a
Prince Vuha <princevuha@gmail.com>
2025-09-14 17:43:47 +0000

    if condition fixed

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -36,7 +36,7 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-	if (typeof salary === "number" && salary < 500) {
+	if (Number(salary) < 500) {
 		return new Teacher();
 	}
 	return new Director();
