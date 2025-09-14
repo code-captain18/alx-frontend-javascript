fc528dfa93570fc57753c160c9fbb7af0c5b60c4
Prince Vuha <princevuha@gmail.com>
2025-09-14 19:10:59 +0000

    crud mods

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,5 +1,9 @@
 
 
-declare function insertRow(row: RowElement): number;
-declare function deleteRow(rowId: RowID): void;
-declare function updateRow(rowId: RowID, row: RowElement): number;
+import { RowID, RowElement } from "./interface";
+
+declare module "./crud.js" {
+  export function insertRow(row: RowElement): number;
+  export function deleteRow(rowId: RowID): void;
+  export function updateRow(rowId: RowID, row: RowElement): number;
+}
--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,18 +1,18 @@
 
 import { RowID, RowElement } from "./interface";
-
+import * as CRUD from "./crud.js";
 
 const row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salva"
 };
 
-const newRowID: RowID = insertRow(row);
+const newRowID: RowID = CRUD.insertRow(row);
 
 const updatedRow: RowElement = {
   ...row,
   age: 23
 };
 
-updateRow(newRowID, updatedRow);
-deleteRow(newRowID);
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
